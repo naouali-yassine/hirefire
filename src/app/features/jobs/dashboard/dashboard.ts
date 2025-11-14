05261e865e63dfdba111ad66e84c94c93a781a2d
Naouali Yassine <yassine.naouali12@gmail.com>
2025-11-14 18:55:58 +0100

    dashboard

--- src/app/features/jobs/dashboard/dashboard.ts
+++ src/app/features/jobs/dashboard/dashboard.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+  navItems = [
+    { icon: '/images/home.svg', route: '/dashboard' },
+    { icon: '/images/swipe.svg', route: '/dashboard/jobs' },
+    { icon: '/images/profile.svg', route: '/dashboard/profile' },
+    { icon: '/images/papers.svg', route: '/dashboard/messages' },
+    { icon: '/images/dashboard.svg', route: '/dashboard/settings' }
+  ];
 
+  constructor(private router: Router) {}
+
+  isActiveRoute(route: string): boolean {
+    return this.router.url === route;
+  }
+
+  logout() {
+  
+    if (typeof window !== 'undefined' && window.localStorage) {
+      localStorage.removeItem('signupUser');
+    }
+    this.router.navigate(['/auth']);
+  }
 }
